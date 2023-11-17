da195ac70bc08f48e0eb7fce2161ff99d624581b
IsaquePFerreira <pferreiraisaque4@gmail.com>
2023-11-17 09:06:04 -0300

    Add ActionOption in MainHeader

--- src/components/MainHeader/index.js
+++ src/components/MainHeader/index.js
@@ -1,11 +1,15 @@
 import "./style.css";
 
 import MainTitle from "../MainTitle";
+import ActionOption from "../ActionOption";
 
 function MainHeader() {
     return `
         <header class="main-header">
         ${MainTitle()}
+        ${ActionOption("#projects", "Projetos")}
+        ${ActionOption("#about", "Sobre")}
+        ${ActionOption("#github", "GitHub")}
         </header>
     `;
 }
