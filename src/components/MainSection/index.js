8bd437d571a604d7df7d055ff60f416c45e270f3
IsaquePFerreira <pferreiraisaque4@gmail.com>
2023-11-21 10:19:24 -0300

    Add MainSubtitle in MainSection

--- src/components/MainSection/index.js
+++ src/components/MainSection/index.js
@@ -1,8 +1,11 @@
 import "./style.css";
 
+import MainSubtitle from "../MainSubtitle";
+
 function MainSection(subtitle = "") {
     return `
         <section class="main-section">
+            ${MainSubtitle(subtitle)}
         </section>
     `;
 }
