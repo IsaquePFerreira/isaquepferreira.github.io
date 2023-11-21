928b6e7d55ae8f864e10129486f6f061d16d85c7
IsaquePFerreira <pferreiraisaque4@gmail.com>
2023-11-21 11:02:11 -0300

    feat: MainSection with dynamic content

--- src/components/MainSection/index.js
+++ src/components/MainSection/index.js
@@ -2,10 +2,11 @@ import "./style.css";
 
 import MainSubtitle from "../MainSubtitle";
 
-function MainSection(subtitle = "") {
+function MainSection(subtitle = "", content = "Aqui vai o conteúdo...") {
     return `
         <section class="main-section">
             ${MainSubtitle(subtitle)}
+            ${content}
         </section>
     `;
 }
