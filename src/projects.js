324d755a0926a9f325c97cccfb69288a0762eda6
IsaquePFerreira <pferreiraisaque4@gmail.com>
2023-11-23 11:18:54 -0300

    Add projects.js

---
+++ src/projects.js
@@ -0,0 +1,10 @@
+const projects = [
+    {
+        url: "https://github.com/IsaquePFerreira/isaquepferreira.github.io",
+        image:  "https://images.pexels.com/photos/211151/pexels-photo-211151.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        altImage: "Imagem Portfólio IsaquePFerreira",
+        title: "IsaquePFerreira"
+    }
+];
+
+export default projects;
