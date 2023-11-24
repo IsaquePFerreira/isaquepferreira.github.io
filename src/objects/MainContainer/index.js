144ec07de2fdfcd40e82b9bada91d7261c7ea1a3
IsaquePFerreira <pferreiraisaque4@gmail.com>
2023-11-24 09:59:04 -0300

    Create MainContainer CSS

--- src/objects/MainContainer/index.js
+++ src/objects/MainContainer/index.js
@@ -1,3 +1,5 @@
+import "./style.css";
+
 import MainSection from "../../components/MainSection";
 import ProjectCard from "../../components/ProjectCard";
 
@@ -7,11 +9,13 @@ const htmlProjectsList = projects.map((project) =>
     ProjectCard(project.url, project.image, project.altImage, project.title));
 const $htmlProjects = htmlProjectsList.join("");
 
-function BodyContent() {
+function MainContainer() {
     return `
-        ${MainSection("Projetos", $htmlProjects)}
-        ${MainSection("Sobre")}
+        <div class="main-container">
+            ${MainSection("Projetos", $htmlProjects)}
+            ${MainSection("Sobre")}
+        </div>
     `;
 }
 
-export default BodyContent;
+export default MainContainer;
