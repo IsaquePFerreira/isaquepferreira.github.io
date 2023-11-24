c1fdaaec93fa201d5d6c95e5383a1c0cb8ab4029
IsaquePFerreira <pferreiraisaque4@gmail.com>
2023-11-24 10:49:01 -0300

    Create ProfilePicture component

---
+++ src/components/ProfilePicture/index.js
@@ -0,0 +1,9 @@
+import "./style.css";
+
+function ProfilePicture(img = "", altImg = "") {
+    return `
+        <img class="profile-picture" src="${img}" alt="${altImg}">
+    `;
+}
+
+export default ProfilePicture;
