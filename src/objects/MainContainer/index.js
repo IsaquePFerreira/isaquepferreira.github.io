070a2760ea3c5dbeda35fdce3106a94289f610af
IsaquePFerreira <pferreiraisaque4@gmail.com>
2023-11-24 10:50:08 -0300

    Add ProfilePicture in MainContainer

--- src/objects/MainContainer/index.js
+++ src/objects/MainContainer/index.js
@@ -2,6 +2,7 @@ import "./style.css";
 
 import MainSection from "../../components/MainSection";
 import ProjectCard from "../../components/ProjectCard";
+import ProfilePicture from "../../components/ProfilePicture";
 
 import projects from "../../projects";
 
@@ -13,7 +14,7 @@ function MainContainer() {
     return `
         <div class="main-container">
             ${MainSection("Projetos", $htmlProjects)}
-            ${MainSection("Sobre")}
+            ${MainSection("Sobre", ProfilePicture("../images/profile-rounded.png", "Imagem Isaque P. Ferreira programador"))}
         </div>
     `;
 }
