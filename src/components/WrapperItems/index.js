af62ee759e45440271499ed2ea1845033feb0a10
IsaquePFerreira <92412320+IsaquePFerreira@users.noreply.github.com>
2023-11-27 11:16:36 -0300

    Create WrapperItems

---
+++ src/components/WrapperItems/index.js
@@ -0,0 +1,11 @@
+import "./style.css";
+
+function WrapperItems(items) {
+    return `
+        <div class="wrapper-items">
+            ${items}
+        </div>
+    `;
+}
+
+export default WrapperItems;
