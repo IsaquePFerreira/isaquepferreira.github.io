c97ae4bd659167f3126c37d150424e6f8726b884
IsaquePFerreira <92412320+IsaquePFerreira@users.noreply.github.com>
2023-11-27 11:17:27 -0300

    Add Wrapper in MainSection

--- src/components/MainSection/index.js
+++ src/components/MainSection/index.js
@@ -1,12 +1,13 @@
 import "./style.css";
 
 import MainSubtitle from "../MainSubtitle";
+import WrapperItems from "../WrapperItems";
 
 function MainSection(subtitle = "", content = "Aqui vai o conteúdo...") {
     return `
         <section class="main-section">
             ${MainSubtitle(subtitle)}
-            ${content}
+            ${WrapperItems(content)}
         </section>
     `;
 }
