b426d4a87cad6032ae85b5a5ec2012465cac254e
IsaquePFerreira <92412320+IsaquePFerreira@users.noreply.github.com>
2023-11-29 11:10:13 -0300

    feat: Create MainFooter component

---
+++ src/components/MainFooter/index.js
@@ -0,0 +1,11 @@
+import "./style.css";
+
+function MainFooter() {
+    return `
+        <footer class="main-footer">
+            <p class="copy">&copy; IsaquePFerreira 2023</p>
+        </footer>
+    `;
+}
+
+export default MainFooter;
