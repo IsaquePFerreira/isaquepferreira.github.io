b1c424d96b2e4ec7c0a99affb62e913ecf6a8c2b
IsaquePFerreira <92412320+IsaquePFerreira@users.noreply.github.com>
2023-12-04 12:13:05 -0300

    Remove Projects section

--- src/objects/MainContent/index.js
+++ src/objects/MainContent/index.js
@@ -1,22 +1,15 @@
 import "./style.css";
 
 import MainSection from "/components/MainSection";
-import ProjectCard from "/components/ProjectCard";
 import ProfilePicture from "/components/ProfilePicture";
 import MainParagraph from "/components/MainParagraph";
 
-import projects from "/projects";
-
-const htmlProjectsList = projects.map((project) =>
-	ProjectCard(project.url, project.image, project.altImage, project.title));
-const $htmlProjects = htmlProjectsList.join("");
-
-const $htmlAbout = ProfilePicture("/images/profile-rounded.png", "Imagem Isaque P. Ferreira programador") + MainParagraph();
+const $htmlAbout = ProfilePicture("/images/profile-rounded.png", "Imagem Isaque P. Ferreira programador") 
+    + MainParagraph("Sou um aspirante a programador e busco me tornar um profissional melhor a cada dia. Meu primeiro contato com tecnologia foi com os jogos, e as poucos fui conhecendo mais sobre a área de informática, tive a oportunidade de fazer meu ensino médio junto com um curso técnico em informática, onde dei meus primeiros passos e onde vi que era a área em queria trabalhar.");
 
 function MainContent() {
 	return `
         <div class="main-content">
-            ${MainSection("Projetos", $htmlProjects)}
             ${MainSection("Sobre", $htmlAbout)}
         </div>
     `;
