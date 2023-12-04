77c8b8d87f46eff2473860e6c42dd6243dba1fe3
IsaquePFerreira <92412320+IsaquePFerreira@users.noreply.github.com>
2023-12-04 12:15:35 -0300

    Create MediaButton component

---
+++ src/components/MediaButton/index.js
@@ -0,0 +1,11 @@
+import "./style.css";
+
+function MediaButton (link = "", img = "", alt = "") {
+    return `
+        <a class="media-button" href="${link}" target="_blank">
+            <img src="${img}" alt="${alt}">
+        </a>
+    `;
+}
+
+export default MediaButton;
