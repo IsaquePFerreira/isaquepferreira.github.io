a30f14bfe4a191a5710a8148e08dfdde340a3bee
IsaquePFerreira <92412320+IsaquePFerreira@users.noreply.github.com>
2024-01-24 14:53:39 -0300

    Create toggleMenu function

---
+++ assets/js/main-nav.js
@@ -0,0 +1,5 @@
+const menuIcon = document.querySelector(".icon-menu");
+const menu = document.querySelector("#menu");
+
+menuIcon.addEventListener('click', () => menu.classList.toggle("-visible"));
+
